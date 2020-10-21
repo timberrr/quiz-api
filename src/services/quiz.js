7ea94d860217b1dc3f33185bb2bbc2ec61302bd7
Timberrr <timberrr@users.noreply.github.com>
2020-10-21 15:07:34 +0100

    fail test - removed player limit

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -276,7 +276,7 @@ async function submitAnswerAsync(roomCode, playerName, answer) {
 }
 
 function start(instance) {
-    if (instance.players.length < 2) {
+    if (instance.players.length < 1) {
         throw new InvalidOperationError('Not enough players yet');
     }
     instance.questionNumber = 1;
